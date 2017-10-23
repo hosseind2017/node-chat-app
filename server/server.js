04c4340e38b78eaabd81b81aaedb4780c35aab2b
hossein <hosseind2017@gmail.com>
2017-10-23 17:26:01 +0330

    Add storing user functions

--- server/server.js
+++ server/server.js
@@ -16,14 +16,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage' , generateMessage('Admin', 'Welcome to the chat app'));
-
-  socket.broadcast.emit('newMessage' , generateMessage('Admin', 'New user joined'));
-
   socket.on('join' , (params, callback) => {
     if (!isRealString(params.name) || !isRealString(params.room)) {
       callback('Name and room are required.')
     }
+
+    socket.join(params.room);
+
+    socket.emit('newMessage' , generateMessage('Admin', 'Welcome to the chat app'));
+    socket.broadcast.to(params.room).emit('newMessage' , generateMessage('Admin', `${params.name} has joined`));
 
     callback();
   });
---
+++ server/utils/users.js
@@ -0,0 +1,31 @@
+
+  class Users {
+    constructor () {
+      this.users = [];
+    }
+    addUser (id, name, room) {
+      var user = {id, name, room};
+      this.users.push(user);
+      return user;
+    }
+    removeUser (id) {
+      var user = this.getUser(id);
+  
+      if (user) {
+        this.users = this.users.filter((user) => user.id !== id);
+      }
+  
+      return user;
+    }
+    getUser (id) {
+      return this.users.filter((user) => user.id === id)[0]
+    }
+    getUserList (room) {
+      var users = this.users.filter((user) => user.room === room);
+      var namesArray = users.map((user) => user.name);
+  
+      return namesArray;
+    }
+  }
+  
+  module.exports = {Users};
