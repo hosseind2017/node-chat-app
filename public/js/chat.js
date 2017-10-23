e7ac43520d94917ddb85f1e56383bf26d8eb59b8
hossein <hosseind2017@gmail.com>
2017-10-23 15:18:52 +0330

    Add validation and test it for work with passing room data

--- public/js/chat.js
+++ public/js/chat.js
@@ -18,7 +18,16 @@ function scrollToBottom () {
 
 
 socket.on('connect', function () {
-  console.log('Connected to server');
+  var params = jQuery.deparam(window.location.search);
+
+  socket.emit('join', params, function (err) {
+    if(err) {
+      alert(err);
+      window.location.href = '/';
+    } else {
+      console.log('No error');
+    }
+  });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -18,6 +19,14 @@ io.on('connection', (socket) => {
   socket.emit('newMessage' , generateMessage('Admin', 'Welcome to the chat app'));
 
   socket.broadcast.emit('newMessage' , generateMessage('Admin', 'New user joined'));
+
+  socket.on('join' , (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      callback('Name and room are required.')
+    }
+
+    callback();
+  });
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
